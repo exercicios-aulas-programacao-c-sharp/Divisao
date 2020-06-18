f36d7f0c6b00f8665ad411f170ee758b43b1365f
diego_neri <diego@diegoneri.com>
2020-06-18 00:13:39 -0300

    Adiciona arquivos após o comando _dotnet new console_

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
